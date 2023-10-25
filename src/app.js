374524a548ef7fa35e7103aca9740047d8e750b2
Alex <aja44@pitt.edu>
2023-10-25 15:10:53 -0400

    added cors to app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.use(cors());
+
 app.use(express.json()); // Add this line to parse JSON request bodies
 
 const moviesRouter = require("./movies/moviesRouter");
